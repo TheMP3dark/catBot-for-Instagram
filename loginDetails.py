6fc9a13850067baac2668c5aabe39cfe40654980
TheMP3dark <41847387+TheMP3dark@users.noreply.github.com>
2020-10-10 18:26:20 +0530

    Create loginDetails.py

---
+++ loginDetails.py
@@ -0,0 +1,8 @@
+class instaLoginDetails:
+    userName = ""
+    passWord = ""
+    apiKey = ""
+    def __init__(self):  
+      self.userName = "USER_NAME"  
+      self.passWord = "PASSWORD"  
+      self.apiKey = "API_KEY"
