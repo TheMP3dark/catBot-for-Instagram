ee3b5e401074b74f700e23156dbf7132adbf3dcb
Mohit Pujari <mohitpujari3@gmail.com>
2020-10-16 11:44:53 +0530

    changed loginDetails

--- loginDetails.py
+++ loginDetails.py
@@ -2,7 +2,8 @@ class instaLoginDetails:
     userName = ""
     passWord = ""
     apiKey = ""
-    def __init__(self):  
-      self.userName = "catapitest"
-      self.passWord = "catchMeIfYouCan!"
-      self.apiKey = "API_KEY"
+
+    def __init__(self):
+        self.userName = "USER_NAME"
+        self.passWord = "PASSWORD"
+        self.apiKey = "API_KEY"
